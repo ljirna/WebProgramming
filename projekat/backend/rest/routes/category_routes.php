5c8cef277608ebaac47af6d72c933b9983327d43
ljirna <mirna.ljiljic@stu.ibu.edu.ba>
2024-06-05 01:44:05 +0200

    Additional Changes

--- projekat/backend/rest/routes/category_routes.php
+++ projekat/backend/rest/routes/category_routes.php
@@ -2,7 +2,22 @@
     require_once __DIR__ . '/../services/CategoryService.class.php';
 
     Flight::set('category_service', new CategoryService());
-
+    
+    /**
+     * @OA\Get(
+     *      path="/categories",
+     *      tags={"categories"},
+     *      summary="Get all categories",
+     *      security={
+     *         {"ApiKey": {}}
+     *      },
+     *      @OA\Response(
+     *           response=200,
+     *           description="Get all categories"
+     *      )
+     * )
+     */
+    
     Flight::route('GET /categories', function() {
         $data = Flight::get('category_service')->get_categories();
         Flight::json(
